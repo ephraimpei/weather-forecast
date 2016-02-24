fdaff6ffc4fd03951b9e32ae9c8932916338e406
Ephraim Pei <ephraimpei@gmail.com>
2016-02-24 01:23:15 -0500

    set up local mongodb connection

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.static(__dirname + '/static'));
 
 require('./config/routes.js')(app);
 
+// connect to MongoDB - test
+require('./db/local_mongodb_connection.js');
+
+// set up server
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
